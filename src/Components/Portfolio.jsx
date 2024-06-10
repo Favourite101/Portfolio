887a7c80e64f6c2e95af03fe710c9052f77ad001
Oluwatunmibi Favour <ofavourite001@gmail.com>
2024-06-10 02:04:03 +0000

    Customize

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/ch7.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Tribble Newsletters",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Led a team of developers to create a newsletter management system for the Tribble community to keep members up to date on the latest news and events. Worked on the backend and frontend of the system.",
+    url: "https://tribblenews.000webhostapp.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "School Management System",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a school management system for schools to manage students, teachers and classes. Worked on the backend and frontend of the system.",
+    url: "https://github.com/Favourite101/School-Management-System",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from Microsoft's resume workshop and deployed to Azure and GitHub pages. Includes my experience and design abilities.",
+    url: "https://favourite101.github.io/Portfolio",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "ToDO List API",
+    description: "Created a RESTful API for a ToDO list application using Python (Django).",
+    url: "https://github.com/Favourite101/ToDO-List-API",
   },
 ];
 
